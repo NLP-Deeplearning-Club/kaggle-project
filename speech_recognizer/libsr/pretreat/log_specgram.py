fcc33d8efdd31c6b0c13bd738c3a11194940adb0
HUANG SIZHE <hsz1273327@gmail.com>
2017-11-30 18:32:12 +0800

    update

--- speech_recognizer/libsr/pretreat/log_specgram.py
+++ speech_recognizer/libsr/pretreat/log_specgram.py
@@ -5,7 +5,7 @@ from scipy import signal
 from scipy.io import wavfile
 
 
-def log_specgram(audio, sample_rate, window='hann', window_size=20,
+def log_specgram(audio, sample_rate, *, window='hann', window_size=20,
                  step_size=10, eps=1e-10):
     """从读出的音频数据中算出对数频谱数据
 
@@ -18,7 +18,7 @@ def log_specgram(audio, sample_rate, window='hann', window_size=20,
         eps (float): - 指明频谱强度取对数时的最小值,防止输入为0后得到负无穷
 
     Returns:
-        tuple[]: - 
+        tuple[np.ndarray,np.ndarray,np.ndarray]: - 由频率(一维),分段时间(一维)和频谱强度(二维)组成的元组
     """
     nperseg = int(round(window_size * sample_rate / 1e3))
     noverlap = int(round(step_size * sample_rate / 1e3))
@@ -31,7 +31,7 @@ def log_specgram(audio, sample_rate, window='hann', window_size=20,
     return freqs, times, np.log(spec.T.astype(np.float32) + eps)
 
 
-def log_specgram_from_path(record_path, window='hann', window_size=20,
+def log_specgram_from_path(record_path, *, window='hann', window_size=20,
                            step_size=10, eps=1e-10):
     """从音频文件读取出对数频谱数据
 
@@ -43,14 +43,11 @@ def log_specgram_from_path(record_path, window='hann', window_size=20,
         eps (float): - 指明频谱强度取对数时的最小值,防止输入为0后得到负无穷
 
     Returns:
-        Iterable: - 压扁后的单层序列
+        tuple[np.ndarray,np.ndarray,np.ndarray]: - 由频率(一维),分段时间(一维)和频谱强度(二维)组成的元组
     """
     sample_rate, samples = wavfile.read(str(record_path))
     freqs, times, log_spec = log_specgram(samples, sample_rate,
                                           window=window,
                                           window_size=window_size,
                                           step_size=step_size, eps=eps)
-    return
-
-
-def to_img
+    return freqs, times, log_spec
