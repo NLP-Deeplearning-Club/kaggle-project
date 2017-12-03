de324d38488aa4d7b2d917324cc60609b36c1943
HUANG SIZHE <hsz1273327@gmail.com>
2017-12-04 00:18:35 +0800

    update

--- speech_recognizer/libsr/pretreat/resample.py
+++ speech_recognizer/libsr/pretreat/resample.py
@@ -15,8 +15,8 @@ def resample(samples, sample_rate, *, new_sample_rate=8000):
     Returns:
         tuple[np.ndarray,np.ndarray]: - 返回结果为重采样后的由频率(一维)和强度(一维)组成的元组
     """
-    resampled = signal.resample(samples, int(
-        new_sample_rate / sample_rate * samples.shape[0]))
+    # int(new_sample_rate / sample_rate * samples.shape[0])
+    resampled = signal.resample(samples, new_sample_rate)
     return new_sample_rate, resampled
 
 
