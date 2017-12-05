081fb3eeba68ed65e844e5f623116d031a32ffb4
Yufan Zheng <yufan.zheng1@gmail.com>
2017-12-05 11:25:53 +0100

    change in denoise.py to make it split by sequence length but not number of split

--- speech_recognizer/libsr/preprocessing/denoising.py
+++ speech_recognizer/libsr/preprocessing/denoising.py
@@ -8,6 +8,54 @@ from glob import glob
 import os
 import numpy as np
 
+def get_main_voice(wav, filter_threshold=0.1, seg_length=800):
+    """ Keep only the main voice in wav
+    
+    Args:
+        wav: The wav data to be processed, in a numeric series list
+        filter_threshold: Threshold to be considered as no signal
+        seg_length: How long is the segment that we will cut the wav for filtering
+        
+    Returns:
+        wav with only the main voice kept
+    """
+    
+    # Split wav into segements
+    wav_length = wav.shape[0]
+    num_seg = int( full_wav_length / seg_length ) + 1
+
+    indice = 0
+    splitted_wavs = []
+
+    while indice + seg_length <= wav_length:
+        w = wav[indice: indice+seg_length]
+        splitted_wavs.append(w)
+        indice += seg_length
+
+    splitted_wavs = np.array(splitted_wavs)
+    
+    # Compute the avg of all segments
+    wavs_mean = []
+    for sw in splitted_wavs:
+        wavs_mean.append(np.absolute(sw).mean())
+    wavs_mean = np.array(wavs_mean)
+
+    # Check if each segments will be kept or not
+    # seg_keep_array : [1 0 1 1 0 1 1 1 1 0 0 0 0 0 0 0 0 0 1 0]
+    seg_keep_array = (wavs_mean > filter_threshold * wavs_mean.max()).astype(int)
+
+    # Find the longgest keep period
+    # [1 0 1 1 0 1 1 1 1 0 0 0 0 0 0 0 0 0 1 0] -> 5, 8 (longest continunous 1)
+    start, end = _get_longest_period(seg_keep_array)
+    if start > 1:
+        start = (start-1)
+    if end < num_seg -1:
+        end = (end+1)
+
+    kept_wav = wav[int(seg_length*start):int(seg_length*end)]
+    return kept_wav
+
+##############################################################################
 
 def _group_consecutives(vals, step=0):
     """Return list of consecutive lists of numbers from vals (number list)."""
@@ -23,6 +71,7 @@ def _group_consecutives(vals, step=0):
         expect = v + step
     return result
 
+##############################################################################
 
 def _get_longest_period(seg_keep_array):
     """获取最长的主干周期"""
@@ -35,47 +84,3 @@ def _get_longest_period(seg_keep_array):
             start += len(cons_wav[i])
         end += len(cons_wav[i])
     return start, end
-
-
-# def get_main_voice(wav, *,filter_threshold=0.1, window=num_seg):
-#     """用于获取主干声音部分
-
-#     Parameters:
-#         wav (np.ndarray): - 指明音频的振幅序列
-#         filter_threshold (float): - 过滤器的阈值
-#         window (int): - 窗口大小,单位为采样次数(以8000hz为例,10ms窗口就是80)
-#         num_seg (int): 
-
-#     Returns:
-#         np.ndarray: - 主干波形
-#     """
-#     """ Keep only the main voice in wav
-
-#     Args:
-#         wav: The wav data to be processed, in a numeric series list
-#         filter_threshold: Threshold to be considered as no signal
-#         num_seg: How many segments we will cut the wav for filtering
-
-#     Returns:
-#         wav with only the main voice kept
-#     """
-
-#     wav = signal.resample(wav, 16000)
-#     seg_length = 16000 / num_seg
-#     splitted_wavs = np.split(wav, num_seg)
-#     wavs_mean = []
-#     for sw in splitted_wavs:
-#         wavs_mean.append(np.absolute(sw).mean())
-#     wavs_mean = np.array(wavs_mean)
-#     seg_keep_array = (wavs_mean > 0.1 * wavs_mean.max()).astype(int)
-
-#     # Find the longgest keep period
-#     # [1 0 1 1 0 1 1 1 1 0 0 0 0 0 0 0 0 0 1 0] -> 5, 8 (longest continunous 1)
-#     start, end = _get_longest_period(seg_keep_array)
-#     if start > 1:
-#         start = (start - 1)
-#     if end < num_seg - 1:
-#         end = (end + 1)
-
-#     keeped_wav = wav[int(seg_length * start):int(seg_length * end)]
-#     return keeped_wav
