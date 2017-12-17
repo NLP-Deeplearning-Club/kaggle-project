bdf1c8b786f1b7bf75317c170f92990f7360c021
Yufan Zheng <yufan.zheng1@gmail.com>
2017-12-17 15:00:42 +0100

    add normalize wav

--- speech_recognizer/libsr/data_augmentation/augmentation_process.py
+++ speech_recognizer/libsr/data_augmentation/augmentation_process.py
@@ -2,7 +2,7 @@ from .steps.mix_noise import mix_background_noise
 from .steps.time_shift import shift_and_pad_zeros
 from .steps.desired_samples import desired_samples_wav
 from .steps.silence_as_zero import silence_as_zero
-
+from .steps.normalize_wav import normalize_wav
 
 def aug_process(wav, label, mode,
                 desired_samples=16000,
@@ -10,6 +10,7 @@ def aug_process(wav, label, mode,
                 background_volume_range=0.1,
                 background_frequency=0.1):
 
+    wav = normalize_wav(wav)
     wav = desired_samples_wav(wav, desired_samples=desired_samples)
     if mode == 'train':
         wav = silence_as_zero(wav, label)
